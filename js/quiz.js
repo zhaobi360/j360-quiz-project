5d64afc7d7eabc24bf1239ade9e91be04baf4533
zhaobi360 <Binwei@Binweis-MacBook-Air.local>
2016-03-30 02:34:52 -0400

    demo1

---
+++ js/quiz.js
@@ -0,0 +1,73 @@
+var score = 0; 
+ function fSubmit() {
+	 var correctanswer = document.getElementById("a1")
+	 if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Your score is now " + score)
+	 }
+	 else {
+		 alert("Incorrect! The correct answer is A! Your score is now " + score)
+	 }		
+ }
+  
+ function fSubmit2() {
+	 var correctanswer = document.getElementById("a7")
+	 if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Your score is now " + score)
+	 }
+	 else {
+		 alert("Incorrect! The correct answer is C! Your score is now " + score)
+	 }
+		
+ }
+ 
+ function fSubmit3() {
+	 var correctanswer = document.getElementById("a9")
+     if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Your score is now " + score)
+	 }
+	 else {
+		 alert("Incorrect! The correct answer is A! Your score is now " + score)
+	 }
+		
+ }
+  
+ function fSubmit4() {
+	 var correctanswer = document.getElementById("a16")
+     if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Your score is now " + score)
+	 }
+	 else {
+		 alert("Incorrect! The correct answer is D! Your score is now " + score)
+	 }
+		
+ }
+ 
+
+ function fSubmit5() {
+	 var correctanswer = document.getElementById("a18")
+     if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Your score is now " + score)
+	 }
+	 else {
+		 alert("Incorrect! The correct answer is B! Your score is now " + score)
+	 }
+		
+ }
+ 
+ function fSubmit6() {
+	 var correctanswer = document.getElementById("a22")
+     if (correctanswer.checked == true) {
+		 score++;
+		 alert("Correct! Cheers! Your total score is " + score)
+	 }
+	 else {
+		 alert("Incorrect! Oops! it should be B! Your total score is " + score)
+	 }
+		
+ }
+ 
